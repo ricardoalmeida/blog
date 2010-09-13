61f21c2e74d9809c27e4e6874e6b50c6dfed325e
tangzero <t4ngz3r0@gmail.com>
2010-09-13 17:43:40 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source "http://rubygems.org"
+
+gem "rails", "3.0.0"
+gem "sqlite3-ruby", :require => "sqlite3"
+gem "haml-rails"
+gem "rails3-generators"
+gem "inherited_resources"
+gem "will_paginate"
+gem "devise"
+gem "simple_form"
+gem "compass"
+gem "highline"
+gem "rspec-rails", ">=2.0.0.beta.20", :group => [:development, :test]
+
+group :test do
+  gem "rspec", ">=2.0.0.beta.20"
+  gem "factory_girl"
+  gem "remarkable_activerecord", ">=4.0.0.alpha4"
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f1ee5096ca9c223135bddce8611e2141cf20821aa187468bcb8840d576077ab4fc61c6bc470cd3cdb6f35705558b7e71adb859c2170a28a21dd1418044ea4311'
