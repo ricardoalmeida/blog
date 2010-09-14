84904c209df3905e528768da5a41a22953d8d52d
tangzero <t4ngz3r0@gmail.com>
2010-09-14 14:22:52 -0300

    add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
+  validates_presence_of :title, :body
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Post do
   should_belong_to :admin
+  should_validate_presence_of :title, :body
 end
